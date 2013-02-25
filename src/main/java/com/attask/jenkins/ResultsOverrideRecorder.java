3c3b143f705e71f9960e974fab27d9232db299c5
Joel <johnson.joel.86@gmail.com>
2013-02-25 10:07:01 -0700

    making it work with matrix jobs.

--- src/main/java/com/attask/jenkins/ResultsOverrideRecorder.java
+++ src/main/java/com/attask/jenkins/ResultsOverrideRecorder.java
@@ -2,6 +2,9 @@ package com.attask.jenkins;
 
 import hudson.Extension;
 import hudson.Launcher;
+import hudson.matrix.MatrixAggregatable;
+import hudson.matrix.MatrixAggregator;
+import hudson.matrix.MatrixBuild;
 import hudson.model.*;
 import hudson.tasks.*;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -13,7 +16,7 @@ import java.io.IOException;
  * Date: 2/17/12
  * Time: 10:26 AM
  */
-public class ResultsOverrideRecorder extends Recorder {
+public class ResultsOverrideRecorder extends Recorder implements MatrixAggregatable {
 	@DataBoundConstructor
 	public ResultsOverrideRecorder() {
 		//nothing
@@ -37,5 +40,14 @@ public class ResultsOverrideRecorder extends Recorder {
 
 	public BuildStepMonitor getRequiredMonitorService() {
 		return BuildStepMonitor.NONE;
+	}
+
+	public MatrixAggregator createAggregator(MatrixBuild build, Launcher launcher, BuildListener listener) {
+		return new MatrixAggregator(build, launcher, listener) {
+			@Override
+			public boolean startBuild() throws InterruptedException, IOException {
+				return perform(build, launcher, listener);
+			}
+		};
 	}
 }
